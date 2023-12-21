c3bbf7189e73112d19fc5b0e84c08bc9d198a0ee
igorlu <igorlu@tencent.com>
2023-12-21 19:07:56 +0800

    update ui logo color

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -167,7 +167,7 @@ export function SideBar(props: { className?: string }) {
           }`}
         >
           <div
-            className={`${styles["sidebar-header"]}  ${styles["sidebar-bg"]}`}
+            className={`${styles["sidebar-header"]} ${styles["sidebar-bg"]}`}
             data-tauri-drag-region
           >
             <div className={styles["sidebar-title"]} data-tauri-drag-region>
@@ -176,11 +176,6 @@ export function SideBar(props: { className?: string }) {
             <div className={`${styles["sidebar-sub-title"]}`}>
               {Locale.Home.Slogn}
             </div>
-            {isMobileScreen && (
-              <div className={styles["sidebar-logo"] + " no-dark"}>
-                <ChatGptIcon />
-              </div>
-            )}
             {!isMobileScreen && (
               <div className={styles["sidebar-toolbar"]}>
                 <IconButton
